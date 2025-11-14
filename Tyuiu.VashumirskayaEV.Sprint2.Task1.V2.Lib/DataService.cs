826492a6e24fbc1696a4dc61b3c7bd9ed771159a
Елизавета Вашумирская <Tyuiu.vashumirskayaEV@outlook.com>
2025-11-14 22:43:19 +0500

    Task1

---
+++ Tyuiu.VashumirskayaEV.Sprint2.Task1.V2.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.VashumirskayaEV.Sprint2.Task1.V2.Lib
+{
+    public class DataService : ISprint2Task1V2
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = a < b;
+            res[1] = b > c;
+            res[2] = c != a;
+            res[3] = d >= b + 10;
+            res[4] = (a + b) < (c + d);
+            res[5] = (a == b) ^ (c > d);
+
+            return res;
+
+        }
+    }
+}
---
+++ Tyuiu.VashumirskayaEV.Sprint2.Task1.V2.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.VashumirskayaEV.Sprint2.Task1.V2.Lib;
+
+namespace Tyuiu.VashumirskayaEV.Sprint2.Task1.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            bool[] res = ds.GetLogicOperations(135, 156, 154, 174);
+            bool[] wait = new bool[6]
+            {
+                true, true, true, true, true, false
+            };
+
+            CollectionAssert.AreEqual(wait, res);
+
+        }
+    }
+}
