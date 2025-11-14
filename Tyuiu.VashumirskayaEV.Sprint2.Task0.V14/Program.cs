f6c9779615a3f65cca855d0976e3dbfdad81f1a3
Елизавета Вашумирская <Tyuiu.vashumirskayaEV@outlook.com>
2025-11-14 22:32:14 +0500

    Add project files.

---
+++ Tyuiu.VashumirskayaEV.Sprint2.Task0.V14/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.VashumirskayaEV.Sprint2.Task0.V14.Lib;
+
+namespace Tyuiu.VashumirskayaEV.Sprint2.Task0.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 1075;
+            int y = 754;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.Title = "Спринт #2 | Выполнила: Вашумирская Е. В. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнила: Вашумирская Елизавета Владимировна | СМАРТб-25-1             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений и арифметических выражений,    *");
+            Console.WriteLine("* которая вернет логическую последовательность.                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ :                                                             *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
