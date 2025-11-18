1869a3c4d04ba8f28d8a6f18acc3731dd28aa504
Елизавета Вашумирская <Tyuiu.vashumirskayaEV@outlook.com>
2025-11-18 19:58:06 +0500

    Task7

--- Tyuiu.VashumirskayaEV.Sprint2.Task7.V12.Lib/DataService.cs
+++ Tyuiu.VashumirskayaEV.Sprint2.Task7.V12.Lib/DataService.cs
@@ -6,11 +6,18 @@ namespace Tyuiu.VashumirskayaEV.Sprint2.Task7.V12.Lib
     {
         public bool CheckDotInShadedArea(double x, double y)
         {
-            bool belowParabola = y <= Math.Pow(x, 2);
-  
-            bool belowLine = y <= 2 - x;
 
-            return belowParabola && belowLine;
+            bool belowParabola = y <= Math.Pow(x, 2);
+            bool aboveParabola = y >= Math.Pow(x, 2);
+
+            bool belowLine = y <= 2 - x;
+            bool aboveLine = y >= 2 - x;
+
+            bool first = belowParabola && aboveLine;
+            bool second = aboveParabola && belowLine;
+
+            return first || second;
+            
         }
     }
 }
